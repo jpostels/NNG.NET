d459662400164d1f9341a3c8ec613008c9296fb5
Jonas Postels <dev@j-postels.de>
2018-03-15 23:17:12 +0100

    TESTS: Add example unit test for Interop.nng_version()

---
+++ tests/NNG.NETTests/Native/InteropTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using NNG.Native;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NNG.NETTests.Native
+{
+    public class InteropTests : TestOutputBase
+    {
+        public InteropTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+        }
+
+        [Fact]
+        public void VersionTest01()
+        {
+            var nngVersionPtr = Interop.nng_version();
+
+            Assert.NotEqual(IntPtr.Zero, nngVersionPtr);
+
+            var nngVersionStr = Marshal.PtrToStringAnsi(nngVersionPtr);
+
+            Assert.NotNull(nngVersionStr);
+            Assert.False(string.IsNullOrWhiteSpace(nngVersionStr), "string.IsNullOrWhiteSpace(nngVersionStr)");
+
+            Print("Version: " + nngVersionStr);
+        }
+
+        [Fact]
+        public void VersionTest02()
+        {
+            const int majorVersion = 0;
+            const int minorVersion = 5;
+            const int patchVersion = 0;
+
+            var nngVersionPtr = Interop.nng_version();
+            var nngVersionStr = Marshal.PtrToStringAnsi(nngVersionPtr);
+
+            Print("Version: " + nngVersionStr);
+
+            var split = nngVersionStr.Split('.');
+
+            Assert.Equal(3, split.Length);
+
+            Assert.True(int.TryParse(split[0], out var major), "int.TryParse(split[0], out var major)");
+            Assert.True(int.TryParse(split[1], out var minor), "int.TryParse(split[1], out var minor)");
+            Assert.True(int.TryParse(split[2], out var patch), "int.TryParse(split[2], out var patch)");
+
+            Assert.Equal(majorVersion, major);
+            Assert.Equal(minorVersion, minor);
+            Assert.Equal(patchVersion, patch);
+        }
+
+        [Fact]
+        public void CloseTest01()
+        {
+            var res = Interop.nng_close(2);
+            Print("HRESULT: " + res);
+        }
+    }
+}
---
+++ tests/NNG.NETTests/TestOutputBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace NNG.NETTests
+{
+    public abstract class TestOutputBase
+    {
+        protected ITestOutputHelper TestOutputHelper { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestOutputBase"/> class.
+        /// </summary>
+        /// <param name="testOutputHelper">The test output helper.</param>
+        protected TestOutputBase(ITestOutputHelper testOutputHelper)
+        {
+            TestOutputHelper = testOutputHelper;
+        }
+
+        /// <summary>
+        ///     Adds a line of text to the output.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        protected void Print(string message) => TestOutputHelper.WriteLine(message);
+
+        /// <summary>
+        ///     Formats a line of text and adds it to the output.
+        /// </summary>
+        /// <param name="format">The message format.</param>
+        /// <param name="args">The format arguments.</param>
+        protected void Print(string format, params object[] args) => TestOutputHelper.WriteLine(format, args);
+    }
+}
