0240b155fb3ff35e1ac89d7e6da354da92444c49
Jonas Postels <dev@j-postels.de>
2018-03-31 01:24:37 +0200

    INTEROP: Add nng_sockaddr_zt struct

---
+++ src/NNG.NET/Native/InteropTypes/nng_sockaddr_zt.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace NNG.Native.InteropTypes
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Using native names of library")]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct nng_sockaddr_zt
+    {
+        public nng_sockaddr_family sa_family;
+
+        public ulong sa_nwid;
+
+        public ulong sa_nodeid;
+
+        public uint sa_port;
+    }
+}
