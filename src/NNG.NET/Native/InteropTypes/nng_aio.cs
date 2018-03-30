7a188124fff92bd01b8af03bc1f0ac75629f97d2
Jonas Postels <dev@j-postels.de>
2018-03-31 01:21:58 +0200

    Interop: Add nng_aio struct

--- src/NNG.NET/Native/InteropTypes/nng_aio.cs
+++ src/NNG.NET/Native/InteropTypes/nng_aio.cs
@@ -8,5 +8,16 @@ namespace NNG.Native.InteropTypes
     public struct nng_aio
     {
         // TODO
+
+        //char* u_rawurl;   // never NULL
+        //char* u_scheme;   // never NULL
+        //char* u_userinfo; // will be NULL if not specified
+        //char* u_host;     // including colon and port
+        //char* u_hostname; // name only, will be "" if not specified
+        //char* u_port;     // port, will be "" if not specified
+        //char* u_path;     // path, will be "" if not specified
+        //char* u_query;    // without '?', will be NULL if not specified
+        //char* u_fragment; // without '#', will be NULL if not specified
+        //char* u_requri;   // includes query and fragment, "" if not specified
     }
 }
