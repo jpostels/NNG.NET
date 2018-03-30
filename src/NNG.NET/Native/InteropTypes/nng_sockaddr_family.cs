3ed0f1637276d9b97643fb40c66db2884350adef
Jonas Postels <dev@j-postels.de>
2018-03-31 01:23:45 +0200

    INTEROP: Add nng_sockaddr_family enum

---
+++ src/NNG.NET/Native/InteropTypes/nng_sockaddr_family.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace NNG.Native.InteropTypes
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Using native names of library")]
+    [SuppressMessage("ReSharper", "EnumUnderlyingTypeIsInt", Justification = "Being explicit here.")]
+    public enum nng_sockaddr_family : ushort
+    {
+        NNG_AF_UNSPEC = 0,
+
+        NNG_AF_INPROC = 1,
+
+        NNG_AF_IPC = 2,
+
+        NNG_AF_INET = 3,
+
+        NNG_AF_INET6 = 4,
+
+        NNG_AF_ZT = 5
+    }
+}
