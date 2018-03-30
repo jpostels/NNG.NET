6d60b2e9616103e02bb31f0199fa06d42bb984c7
Jonas Postels <dev@j-postels.de>
2018-03-31 01:25:20 +0200

    INTEROP: Add nng_unit_enum enum

---
+++ src/NNG.NET/Native/InteropTypes/nng_unit_enum.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace NNG.Native.InteropTypes
+{
+    /// <summary>
+    ///     nng_stat_unit provides information about the unit for the statistic,
+    ///     such as NNG_UNIT_BYTES or NNG_UNIT_BYTES.  If no specific unit is
+    ///     applicable, such as a relative priority, then NN_UNIT_NONE is
+    ///     returned.
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Using native names of library")]
+    [SuppressMessage("ReSharper", "EnumUnderlyingTypeIsInt", Justification = "Being explicit here.")]
+    public enum nng_unit_enum : int
+    {
+        NNG_UNIT_NONE = 0,
+
+        NNG_UNIT_BYTES = 1,
+
+        NNG_UNIT_MESSAGES = 2,
+
+        NNG_UNIT_BOOLEAN = 3,
+
+        NNG_UNIT_MILLIS = 4,
+
+        NNG_UNIT_EVENTS = 5
+    }
+}
