cdefec5ba8d9c6fff82387ec93669f68f5bf8ad8
Jonas Postels <dev@j-postels.de>
2018-05-10 18:00:11 +0200

    TESTS: Add integration test stub

---
+++ tests/IntegrationTests/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Transactions;
+using NNG.Native;
+
+namespace IntegrationTests
+{
+    public static class Program
+    {
+        public const string PipeName = "integrationTest01";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Running integration test #1");
+
+            Console.Write("Initialize NNG... ");
+            Interop.Initialize();
+            Console.Write("DONE! \n");
+
+            CreateReplySocket();
+            CreateRequestSocket();
+        }
+
+        private static void CreateReplySocket()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CreateRequestSocket()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
